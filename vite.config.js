2a32d3d9efb03859795810cdc6b2460612a52f9f
wnvei <wvei.biz@gmail.com>
2025-07-23 18:17:44 +0530

    updated for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/your-companion/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/your-companion/',
 })
